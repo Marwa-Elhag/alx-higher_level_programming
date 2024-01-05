b8721386336e06a0ba6e10ec047aa4fd51649878
Marwa Elhag <marwaalhaj89@gmail.com>
2024-01-05 01:49:45 -0800

    A

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,6 +1,13 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    from add_0 import add
-    a = 1
-    b = 2
-    print("{} + {} = {}".format(a, b, add(a, b)))
+    import sys
+
+    count = len(sys.argv) - 1
+    if count == 0:
+        print("0 arguments.")
+    elif count == 1:
+        print("1 argument:")
+    else:
+        print("{} arguments:".format(count))
+        for i in range(count):
+            print("{}: {}".format(i +1, sys.argv[i + 1]))
